225f1c0573bad1c829a42c7b32d8f9a9dd7df18f
Merim Kulovac <92273186+mkulovac1@users.noreply.github.com>
2023-08-25 20:22:54 +0200

    title to parmalink

--- admin-dashboard/src/app/posts/new-post/new-post.component.ts
+++ admin-dashboard/src/app/posts/new-post/new-post.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+  permalink: string = ''
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  onTitleChanged(event) {
+    const title = event.target.value;
+    this.permalink = title.replace(/\s/g, '-')
+    // console.log(this.permalink);
+  }
 }
